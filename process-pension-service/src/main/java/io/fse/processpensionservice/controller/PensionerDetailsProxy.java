a0695eeed010214b457439c79d31fe5560d07d41
Shubham Nagre <shubhamnagre14@gmail.com>
2022-05-17 00:45:23 +0530

    Added JWT authentication in Process-Pension-Service for POST method

--- process-pension-service/src/main/java/io/fse/processpensionservice/controller/PensionerDetailsProxy.java
+++ process-pension-service/src/main/java/io/fse/processpensionservice/controller/PensionerDetailsProxy.java
@@ -4,11 +4,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import io.fse.processpensionservice.model.Pension;
 
 @FeignClient(name = "pensioner-detail", url = "localhost:8000")
 public interface PensionerDetailsProxy {
+
+	//	public ResponseEntity<Pension> findPensionerByAadhaar(@PathVariable long aadhaarNumber);
+	
 	@GetMapping("/PensionerDetailByAadhaar/{aadhaarNumber}")
-	public ResponseEntity<Pension> findPensionerByAadhaar(@PathVariable long aadhaarNumber);
+	public ResponseEntity<Pension> findPensionerByAadhaar(@RequestHeader("Authorization") String token, @PathVariable long aadhaarNumber);
 }
--- process-pension-service/src/main/java/io/fse/processpensionservice/controller/ProcessPensionController.java
+++ process-pension-service/src/main/java/io/fse/processpensionservice/controller/ProcessPensionController.java
@@ -3,23 +3,55 @@ package io.fse.processpensionservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.fse.processpensionservice.model.Pension;
+import io.fse.processpensionservice.service.JwtUtil;
 
 @RestController
+@CrossOrigin
 public class ProcessPensionController {
 	@Autowired
 	PensionerDetailsProxy proxy;
 	
+	@Autowired
+	private JwtUtil JwtUtil;
+	
+//	@PostMapping("/ProcessPension/{aadhaarNumber}")
+//	public ResponseEntity<Pension> getPensionDetails(@PathVariable long aadhaarNumber) {
+//		ResponseEntity<Pension> response = proxy.findPensionerByAadhaar(aadhaarNumber);
+//		
+//		Pension pension = response.getBody();
+//		
+//		// Calculate Pension Amount
+//		if (pension.getPensionType().equalsIgnoreCase("self")) {
+//			System.out.println("Inside self");
+//			pension.setPensionAmount((0.8 * pension.getSalaryEarned()) + pension.getAllowances());
+//		} else if (pension.getPensionType().equalsIgnoreCase("family")) {
+//			pension.setPensionAmount((0.5 * pension.getSalaryEarned()) + pension.getAllowances());
+//		}
+//		
+//		// Get Service charges for bank
+//		if (pension.getBankType().equalsIgnoreCase("public")) {
+//			pension.setServiceCharges(500d);
+//		} else if (pension.getBankType().equalsIgnoreCase("private")) {
+//			pension.setServiceCharges(550d);
+//		}
+//		
+//		return new ResponseEntity<Pension>(pension, HttpStatus.CREATED);
+//	}
+	
 	@PostMapping("/ProcessPension/{aadhaarNumber}")
-	public ResponseEntity<Pension> getPensionDetails(@PathVariable long aadhaarNumber) {
-		ResponseEntity<Pension> response = proxy.findPensionerByAadhaar(aadhaarNumber);
+	public ResponseEntity<Pension> getPensionDetails(@RequestHeader("Authorization") String token, @PathVariable long aadhaarNumber) {
 		
+		ResponseEntity<Pension> response = proxy.findPensionerByAadhaar(token, aadhaarNumber);
+
 		Pension pension = response.getBody();
-		
+
 		// Calculate Pension Amount
 		if (pension.getPensionType().equalsIgnoreCase("self")) {
 			System.out.println("Inside self");
@@ -27,14 +59,14 @@ public class ProcessPensionController {
 		} else if (pension.getPensionType().equalsIgnoreCase("family")) {
 			pension.setPensionAmount((0.5 * pension.getSalaryEarned()) + pension.getAllowances());
 		}
-		
+
 		// Get Service charges for bank
 		if (pension.getBankType().equalsIgnoreCase("public")) {
 			pension.setServiceCharges(500d);
 		} else if (pension.getBankType().equalsIgnoreCase("private")) {
 			pension.setServiceCharges(550d);
 		}
-		
+
 		return new ResponseEntity<Pension>(pension, HttpStatus.CREATED);
 	}
 }
